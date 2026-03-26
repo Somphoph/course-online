f7115f4a33b93e76aeb0fb28cf4dec0cba173711
Somphop Homprasert <somphop.homprasert@krungthai.com>
2026-03-26 12:52:33 +0700

    feat: add social login fields and replace hashed cast with nullable-safe Attribute

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -13,40 +15,42 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'phone',
         'role',
         'password',
+        'google_id',
+        'facebook_id',
+        'avatar',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
         ];
+    }
+
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: function (?string $value): ?string {
+                if (is_null($value)) {
+                    return null;
+                }
+                if (Hash::isHashed($value)) {
+                    return $value;
+                }
+                return Hash::make($value);
+            },
+        );
     }
 
     public function isAdmin(): bool
